a700299b7d3b589fe1e4da3af3b78183dae314a8
BoccoSantiago <italobocco@gmail.com>
2022-10-19 15:24:19 +0200

    added styles

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import TaskForm from './components/TaskForm';
 import TasksList from './components/TasksList';
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
@@ -7,14 +6,20 @@ function App() {
 
   return (
   <div className="App">
-    <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<TasksList/>}/>
-      <Route path='/create-task' element={<TaskForm/>}/>
-      <Route path='/edit-task/:id' element={<TaskForm/>}/>
-    </Routes>
-    </BrowserRouter> 
+    <div className="bg-zinc-900 h-screen text-white">
+      <div className="flex items-center justify-center h-full">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<TasksList />} />
+            <Route path="/create-task" element={<TaskForm />} />
+            <Route path="/edit-task/:id" element={<TaskForm />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </div>
   </div>)
 }
 
 export default App;
+
+
--- src/components/TasksList.js
+++ src/components/TasksList.js
@@ -1,33 +1,53 @@
-import React from 'react'
-import {useSelector, useDispatch} from 'react-redux'
-import {deleteTask} from '../features/tasks/tasksSlice'
-import {Link} from 'react-router-dom'
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { deleteTask } from "../features/tasks/tasksSlice";
+import { Link } from "react-router-dom";
 
 function TasksList() {
-   const tasks =  useSelector(state => state.tasks)
-   const dispatch = useDispatch()
-    
-   const handleDelete = (id) => {
-    dispatch(deleteTask(id))
-   }
+  const tasks = useSelector((state) => state.tasks);
+  const dispatch = useDispatch();
+
+  const handleDelete = (id) => {
+    dispatch(deleteTask(id));
+  };
 
   return (
-    <div>
-        <header>
-            <h1>task {tasks.length}</h1>
-            <Link to='/create-task'>Create Task</Link>
-
-        </header>
-        {tasks.map(task => (
-            <div key={task.id}>
-            <h3>{task.title}</h3>
-            <p>{task.description}</p>
-            <button onClick={()=>handleDelete(task.id)}>Delete</button>
-            <Link to={`/edit-task/${task.id}`}>Edit</Link>
-            </div>
+    <div className="w-4/6">
+      <header className="flex justify-between items-center py-4">
+        <h1>Tasks: {tasks.length}</h1>
+        <Link
+          to="/create-task"
+          className="bg-indigo-800 px-2 py-1 rounded-sm text-sm"
+        >
+          Create Task
+        </Link>
+      </header>
+      <div className="grid lg:grid-cols-3 gap-3">
+        {tasks.map((task) => (
+          <div key={task.id} className="bg-neutral-800 p-4 rounded-md min-w-full">
+            <header className="flex justify-between">
+              <h3>{task.title}</h3>
+              <div className="flex gap-x-2">
+                <Link
+                  to={`/edit-task/${task.id}`}
+                  className="bg-zinc-600 px-2 py-1 text-xs rounded-md self-center"
+                >
+                  Edit
+                </Link>
+                <button
+                  onClick={() => handleDelete(task.id)}
+                  className="bg-red-500 px-2 py-1 text-xs rounded-md "
+                >
+                  Delete
+                </button>
+              </div>
+            </header>
+            <p className="text-xs text-slate-400">{task.description}</p>
+          </div>
         ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default TasksList
+export default TasksList;
